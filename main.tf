92a23aab9749cd95f9bfbfc4246fd72afb903079
Erwan78100 <erwan.blan2003@gmail.com>
2025-12-26 18:01:22 +0100

    Iam setup ajout d'un utilisateur appartenant au group auditeur

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 provider "aws" {
   region = "eu-west-3" # Paris, cohérent avec ton plan IDF
 }
+resource "aws_iam_user_group_membership" "equipe_auditeur" {
+user=aws_iam_user.user1.name
+groups=[
+aws_iam_group.auditors.name
+]
+}
+resource "aws_iam_user" "user1" {
+name="erwan_dev"
+}
 resource "aws_iam_group" "auditors" {
 name ="audit-team"
 }
